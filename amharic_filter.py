2e91ff747e8979ee136c27062e9b699eb345f084
Surafeljava <surafelk27@gmail.com>
2022-06-19 15:24:22 +0300

    feat: get data with username done

--- amharic_filter.py
+++ amharic_filter.py
@@ -4,22 +4,26 @@ from aiohttp import ClientSession
 # sys.path.append('/var/www/webApp/webApp')
 
 baseUrl = 'https://api.twitter.com/1.1/statuses/show.json'
+userTimelineBaseUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+storageBaseUrl = 'https://asa2022.herokuapp.com'
 
 
 class AmharicFilter:
     def __init__(self):
         self.sentence = ''
-        self.location = ''
+        self.lat = ''
+        self.lng = ''
         self.country = ''
-        self.amharic_letters = ['ሀ', 'ሁ', 'ሂ', 'ሃ', 'ሄ', 'ህ', 'ሆ', 'ሇ', 'ለ', 'ሉ', 'ሊ', 'ላ', 'ሌ', 'ል', 'ሎ', 'ሏ', 'ሐ', 'ሑ', 'ሒ', 'ሓ', 'ሔ', 'ሕ', 'ሖ', 'ሗ', 'መ', 'ሙ', 'ሚ', 'ማ', 'ሜ', 'ም', 'ሞ', 'ሟ', 'ሠ', 'ሡ', 'ሢ', 'ሣ', 'ሤ', 'ሥ', 'ሦ', 'ሧ', 'ረ', 'ሩ', 'ሪ', 'ራ', 'ሬ', 'ር', 'ሮ', 'ሯ', 'ሰ', 'ሱ', 'ሲ', 'ሳ', 'ሴ', 'ስ', 'ሶ', 'ሷ', 'ሸ', 'ሹ', 'ሺ', 'ሻ', 'ሼ', 'ሽ', 'ሾ', 'ሿ', 'ቀ', 'ቁ', 'ቂ', 'ቃ', 'ቄ', 'ቅ', 'ቆ', 'ቇ', 'ቈ', '቉', 'ቊ', 'ቋ', 'ቌ', 'ቍ', '቎', '቏', 'ቐ', 'ቑ', 'ቒ', 'ቓ', 'ቔ', 'ቕ', 'ቖ', '቗', 'ቘ', '቙', 'ቚ', 'ቛ', 'ቜ', 'ቝ', '቞', '቟', 'በ', 'ቡ', 'ቢ', 'ባ', 'ቤ', 'ብ', 'ቦ', 'ቧ', 'ቨ', 'ቩ', 'ቪ', 'ቫ', 'ቬ', 'ቭ', 'ቮ', 'ቯ', 'ተ', 'ቱ', 'ቲ', 'ታ', 'ቴ', 'ት', 'ቶ', 'ቷ', 'ቸ', 'ቹ', 'ቺ', 'ቻ', 'ቼ', 'ች', 'ቾ', 'ቿ', 'ኀ', 'ኁ', 'ኂ', 'ኃ', 'ኄ', 'ኅ', 'ኆ', 'ኇ', 'ኈ', '኉', 'ኊ', 'ኋ', 'ኌ', 'ኍ', '኎', '኏', 'ነ', 'ኑ', 'ኒ', 'ና', 'ኔ', 'ን', 'ኖ', 'ኗ', 'ኘ', 'ኙ', 'ኚ', 'ኛ', 'ኜ', 'ኝ', 'ኞ', 'ኟ', 'አ', 'ኡ', 'ኢ', 'ኣ', 'ኤ', 'እ', 'ኦ', 'ኧ', 'ከ', 'ኩ', 'ኪ', 'ካ', 'ኬ', 'ክ', 'ኮ', 'ኯ', 'ኰ', '኱', 'ኲ', 'ኳ', 'ኴ', 'ኵ', '኶', '኷', 'ኸ', 'ኹ', 'ኺ', 'ኻ', 'ኼ', 'ኽ', 'ኾ', '኿', 'ዀ', '዁', 'ዂ', 'ዃ', 'ዄ', 'ዅ', '዆', '዇',
+        self.amharic_letters = [' ', 'ሀ', 'ሁ', 'ሂ', 'ሃ', 'ሄ', 'ህ', 'ሆ', 'ሇ', 'ለ', 'ሉ', 'ሊ', 'ላ', 'ሌ', 'ል', 'ሎ', 'ሏ', 'ሐ', 'ሑ', 'ሒ', 'ሓ', 'ሔ', 'ሕ', 'ሖ', 'ሗ', 'መ', 'ሙ', 'ሚ', 'ማ', 'ሜ', 'ም', 'ሞ', 'ሟ', 'ሠ', 'ሡ', 'ሢ', 'ሣ', 'ሤ', 'ሥ', 'ሦ', 'ሧ', 'ረ', 'ሩ', 'ሪ', 'ራ', 'ሬ', 'ር', 'ሮ', 'ሯ', 'ሰ', 'ሱ', 'ሲ', 'ሳ', 'ሴ', 'ስ', 'ሶ', 'ሷ', 'ሸ', 'ሹ', 'ሺ', 'ሻ', 'ሼ', 'ሽ', 'ሾ', 'ሿ', 'ቀ', 'ቁ', 'ቂ', 'ቃ', 'ቄ', 'ቅ', 'ቆ', 'ቇ', 'ቈ', '቉', 'ቊ', 'ቋ', 'ቌ', 'ቍ', '቎', '቏', 'ቐ', 'ቑ', 'ቒ', 'ቓ', 'ቔ', 'ቕ', 'ቖ', '቗', 'ቘ', '቙', 'ቚ', 'ቛ', 'ቜ', 'ቝ', '቞', '቟', 'በ', 'ቡ', 'ቢ', 'ባ', 'ቤ', 'ብ', 'ቦ', 'ቧ', 'ቨ', 'ቩ', 'ቪ', 'ቫ', 'ቬ', 'ቭ', 'ቮ', 'ቯ', 'ተ', 'ቱ', 'ቲ', 'ታ', 'ቴ', 'ት', 'ቶ', 'ቷ', 'ቸ', 'ቹ', 'ቺ', 'ቻ', 'ቼ', 'ች', 'ቾ', 'ቿ', 'ኀ', 'ኁ', 'ኂ', 'ኃ', 'ኄ', 'ኅ', 'ኆ', 'ኇ', 'ኈ', '኉', 'ኊ', 'ኋ', 'ኌ', 'ኍ', '኎', '኏', 'ነ', 'ኑ', 'ኒ', 'ና', 'ኔ', 'ን', 'ኖ', 'ኗ', 'ኘ', 'ኙ', 'ኚ', 'ኛ', 'ኜ', 'ኝ', 'ኞ', 'ኟ', 'አ', 'ኡ', 'ኢ', 'ኣ', 'ኤ', 'እ', 'ኦ', 'ኧ', 'ከ', 'ኩ', 'ኪ', 'ካ', 'ኬ', 'ክ', 'ኮ', 'ኯ', 'ኰ', '኱', 'ኲ', 'ኳ', 'ኴ', 'ኵ', '኶', '኷', 'ኸ', 'ኹ', 'ኺ', 'ኻ', 'ኼ', 'ኽ', 'ኾ', '኿', 'ዀ', '዁', 'ዂ', 'ዃ', 'ዄ', 'ዅ', '዆', '዇',
                                 'ወ', ' ዉ', 'ዊ', 'ዋ', 'ዌ', 'ው', 'ዎ', 'ዏ', 'ዐ', 'ዑ', 'ዒ', 'ዓ', 'ዔ', 'ዕ', 'ዖ', '዗', 'ዘ', 'ዙ', 'ዚ', 'ዛ', 'ዜ', 'ዝ', 'ዞ', 'ዟ', 'ዠ', 'ዡ', 'ዢ', 'ዣ', 'ዤ', 'ዥ', 'ዦ', 'ዧ', 'የ', 'ዩ', 'ዪ', 'ያ', 'ዬ', 'ይ', 'ዮ', 'ዯ', 'ደ', 'ዱ', 'ዲ', 'ዳ', 'ዴ', 'ድ', 'ዶ', 'ዷ', 'ዸ', 'ዹ', 'ዺ', 'ዻ', 'ዼ', 'ዽ', 'ዾ', 'ዿ', 'ጀ', 'ጁ', 'ጂ', 'ጃ', 'ጄ', 'ጅ', 'ጆ', 'ጇ', 'ገ', 'ጉ', 'ጊ', 'ጋ', 'ጌ', 'ግ', 'ጎ', 'ጏ', 'ጐ', '጑', 'ጒ', 'ጓ', 'ጔ', 'ጕ', '጖', '጗', 'ጘ', 'ጙ', 'ጚ', 'ጛ', 'ጜ', 'ጝ', 'ጞ', 'ጟ', 'ጠ', 'ጡ', 'ጢ', 'ጣ', 'ጤ', 'ጥ', 'ጦ', 'ጧ', 'ጨ', 'ጩ', 'ጪ', 'ጫ', 'ጬ', ' ጭ', 'ጮ', 'ጯ', 'ጰ', 'ጱ', 'ጲ', 'ጳ', 'ጴ', 'ጵ', 'ጶ', 'ጷ', 'ጸ', 'ጹ', 'ጺ', 'ጻ', 'ጼ', 'ጽ', 'ጾ', 'ጿ', 'ፀ', 'ፁ', 'ፂ', 'ፃ', 'ፄ', 'ፅ', 'ፆ', 'ፇ', 'ፈ', 'ፉ', 'ፊ', 'ፋ', 'ፌ', 'ፍ', 'ፎ', 'ፏ', 'ፐ', 'ፑ', 'ፒ', 'ፓ', 'ፔ', 'ፕ', 'ፖ', 'ፗ', 'ፘ', 'ፙ', 'ፚ', '፛', '፜', '፝', '፞', '፟', '፠', '፡', '።', '፣', '፤', '፥', '፦', '፧', '፨', '፩', '፪', '፫', '፬', '፭', '፮', '፯', '፰', '፱', '፲', '፳', '፴', '፵', '፶', '፷', '፸', '፹', '፺', '፻', '፼', '፽', '፾', '፿', 'ᎀ', 'ᎁ', 'ᎂ', 'ᎃ', 'ᎄ', 'ᎅ', 'ᎆ', 'ᎇ', 'ᎈ', 'ᎉ', 'ᎊ', 'ᎋ', 'ᎌ', 'ᎍ', 'ᎎ', 'ᎏ', '᎐', '።', '፣']
 
     def setData(self, sentence, country):
         self.sentence = sentence
         self.country = country
 
-    def setLocation(self, location):
-        self.location = location
+    def setLocation(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
 
     def getLocation(self):
         return self.location
@@ -27,7 +31,8 @@ class AmharicFilter:
     def getData(self):
         return {
             "sentence": self.sentence,
-            "location": self.location,
+            "lat": self.lat,
+            "lng": self.lng,
             "country": self.country
         }
 
@@ -137,10 +142,49 @@ class AmharicFilter:
 
         return check
 
+    def removeNonAmharic(self):
+        # function to remove non amharic letters from sentence
+        new_text = ''
+
+        for l in self.sentence:
+            if l in self.amharic_letters:
+                new_text += l
+
+        self.sentence = new_text
+        return new_text
+
     async def get_data(self, tweet_id):
         endpoint = f'?id={tweet_id}'
 
         headers = {"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAJ0PXQEAAAAAHBTso%2BOW1k6V5kgGzCfIGLMbUpg%3DvXjswixg0EJ3sgt1uBL78uyeJDfwyAcpXkHjE34aaPsBc07pv6"}
-        async with ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(f'{baseUrl}{endpoint}', headers=headers) as response:
                 return await response.json()
+
+    async def get_tweet_by_username(self, username):
+        endpoint = f'?screen_name={username}'
+
+        headers = {"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAJ0PXQEAAAAAHBTso%2BOW1k6V5kgGzCfIGLMbUpg%3DvXjswixg0EJ3sgt1uBL78uyeJDfwyAcpXkHjE34aaPsBc07pv6"}
+        async with ClientSession(trust_env=True) as session:
+            async with session.get(f'{userTimelineBaseUrl}{endpoint}', headers=headers) as response:
+                return await response.json()
+
+    async def save_to_hate_collection(self, data):
+        endpoint = f'/api/saved_tweets/create_analyzed_tweet'
+
+        async with ClientSession(trust_env=True) as session:
+            async with session.post(f'{storageBaseUrl}{endpoint}', json=data) as response:
+                return await response.json()
+
+    async def check_if_tweet_collected_before(self, tweet_id):
+        endpoint = f'/api/saved_tweets/single_analyzed_tweet/{tweet_id}/'
+
+        async with ClientSession(trust_env=True) as session:
+            async with session.get(f'{storageBaseUrl}{endpoint}') as response:
+                try:
+                    res = await response.json()
+                    return False
+
+                except Exception as e:
+                    return True
+                # return await response.json()
--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 # from flask_cors import CORS, cross_origin
 from flask import Flask, request, jsonify
 from aiohttp import ClientSession
+from datetime import date
 
 from amharic_filter import AmharicFilter
 from utils import Utils
-
+import random
 # import joblib
 # from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -37,7 +38,7 @@ async def get_address(address):
     endpoint = f'?address={address}&key=AIzaSyABPEzN748CdxwYb0FHLP9RTkZ7-FE_4pw'
 
     # headers = {"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAJ0PXQEAAAAAHBTso%2BOW1k6V5kgGzCfIGLMbUpg%3DvXjswixg0EJ3sgt1uBL78uyeJDfwyAcpXkHjE34aaPsBc07pv6"}
-    async with ClientSession() as session:
+    async with ClientSession(trust_env=True) as session:
         async with session.get(f'{addressBaseUrl}{endpoint}') as response:
             return await response.json()
 
@@ -74,8 +75,93 @@ def predict():
         return jsonify({'sentence': sentence, 'error': e}), 400
 
 
+@app.route('/predict_from_username', methods=['POST'])
+async def predict_from_username():
+    try:
+        data = request.json
+        username = data['username']
+        # recent = data['recent']
+
+        # Fetch user posts
+        amf = AmharicFilter()
+        res = await amf.get_tweet_by_username(username)
+
+        if len(res) < 0:
+            return jsonify({'error': 'No post for this username'}), 400
+
+        # print(res)
+
+        # Only choosing the recent 5 posts of the user
+        recent_five = [{'tweetId': a.get('id'), 'text': a.get('text'),
+                        'country': a['user'].get('location'), 'dateOfTweet': a.get('created_at')} for a in res[0:5]]
+
+        # print(recent_five)
+
+        contains_hate = []
+
+        for tw in recent_five:
+
+            if not tw['text']:
+                continue
+
+            # update sentence with the one fetched from the API
+            amf.setData(tw['text'], tw['country'])
+            d = amf.getData()
+
+            is_amh = amf.checkIfAmharic()
+            if not is_amh:
+                continue
+
+            # remove non amharic letters
+            all_amh = amf.removeNonAmharic()
+
+            # normalize the sentence
+            normalized = amf.normalize()
+
+            # Check for hate speech here
+            hate_check_res = predict_from_sentence(normalized)
+
+            # Check if it has country or city name
+            address = amf.getData()['country'] + ''
+            if address != '':
+                # Add the new location
+                loc = await get_address(address)
+                l = loc['results'][0]['geometry']['location']
+                amf.setLocation(l['lat'], l['lng'])
+
+            # Update the text with the normalized one
+            tm = tw
+            tm['text'] = normalized
+            tm['hateValue'] = hate_check_res
+            tm['dateOfPrediction'] = date.today().strftime("%b-%d-%Y")
+            tm['loc_lat'] = amf.getData()['lat'] + random.randint(2, 8)
+            tm['loc_lng'] = amf.getData()['lng'] + random.randint(1, 5)
+            # tm['dateOfTweet'] = hate_check_res['prediction']
+            contains_hate.append(tm)
+            # print(amf.check_hatespeech())
+
+        new_tweets = []
+
+        # for all in contains_hate: check if already collected
+        for tweet in contains_hate:
+            check = await amf.check_if_tweet_collected_before(tweet['tweetId'])
+            if check and tweet['hateValue'] > 0.49 and tweet['country'] != '':
+                new_tweets.append(tweet)
+
+        # print(new_tweets)
+
+        # for all in contains_hate: save them to the database
+        for tweet_info in new_tweets:
+            res = await amf.save_to_hate_collection(tweet_info)
+            print(res, '\n')
+
+        return jsonify({'alltweet': contains_hate}), 200
+    except Exception as e:
+        return jsonify({'error': e}), 400
+
+
 @app.route('/predict_from_tweetid', methods=['GET', 'POST'])
-async def get_tweet_data():
+async def predict_from_tweetid():
     if request.method == 'GET':
         return jsonify({'message': 'Method Not Allowed'}), 405
     if request.method == 'POST':
@@ -106,17 +192,37 @@ async def get_tweet_data():
         normalized = amf.normalize()
 
         # Check if it has country or city name
+
+        print(res)
+
         address = d['country']
         if address != '':
             # Add the new location
             loc = await get_address(address)
-            l = loc['results'][0]['geometry']['location']
-            amf.setLocation(l)
+            l = res['geometry']['location']
+            amf.setLocation(l['lat'], l['lng'])
 
         print(amf.getData())
 
         try:
             pr = predict_from_sentence(normalized)
+
+            data = {
+                'tweetId': t_id,
+                'text': normalized,
+                'hateValue': pr,
+                'dateOfPrediction': date.today().strftime("%b-%d-%Y"),
+                'loc_lat': amf.getData()['lat'],
+                'loc_lng': amf.getData()['lng'],
+                'country': res['user'].get('location'),
+                'dateOfTweet': res.get('created_at')
+            }
+
+            check = await amf.check_if_tweet_collected_before(t_id)
+            if check and pr > 0.49 and data['country'] != '':
+                res = await amf.save_to_hate_collection(data)
+                # new_tweets.append(tweet)
+
             return jsonify({'sentence': normalized, 'prediction': pr, 'tweet_data': amf.getData()}), 200
         except Exception as e:
             return jsonify({'sentence': normalized, 'error': e}), 400
